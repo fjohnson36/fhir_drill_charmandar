44dc9a106a42c756b2769025cfdb84bf1f5a2db4
fjohnson36 <fjohnson36@gatech.edu>
2015-10-29 23:18:09 -0400

    Update register.server.controller.js
    commit

--- app/controllers/register.server.controller.js
+++ app/controllers/register.server.controller.js
@@ -1,29 +1,13 @@
-(function () {
-    'use strict';
-
-    angular
-        .module('app')
-        .controller('RegisterController', RegisterController);
-
-    RegisterController.$inject = ['UserService', '$location', '$rootScope', 'FlashService'];
-    function RegisterController(UserService, $location, $rootScope, FlashService) {
-        var vm = this;
-
-        vm.register = register;
-
-        function register() {
-            vm.dataLoading = true;
-            UserService.Create(vm.user)
-                .then(function (response) {
-                    if (response.success) {
-                        FlashService.Success('Registration successful', true);
-                        $location.path('/login');
-                    } else {
-                        FlashService.Error(response.message);
-                        vm.dataLoading = false;
-                    }
-                });
-        }
-    }
-
-})();
+exports.render = function(req, res) {
+	if (req.session.lastVisit) {
+		console.log(req.session.lastVisit);		
+	}
+	
+	req.session.lastVisit = new Date();
+	
+	res.render('register', {
+		title: 'Hello World',
+		result: '',
+		data: ''
+	})
+};
