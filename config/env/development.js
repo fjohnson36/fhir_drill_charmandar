6caba63d36a22ab578840e32e897b438a1244ba8
fjohnson36 <fjohnson36@gatech.edu>
2015-11-10 16:25:12 -0500

    Update development.js

--- config/env/development.js
+++ config/env/development.js
@@ -4,4 +4,5 @@ module.exports = {
 	db: 'mongodb://charmander:fhir@ds047652.mongolab.com:47652/charmanderdev',
 	fhir: 'http://polaris.i3l.gatech.edu:8080/gt-fhir-webapp-ro/base/',
 	sessionSecret: 'developmentSessionSecret'
+	sessionTimeOutDuration: 300000
 }
