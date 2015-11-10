f13c359b19b77501122868beaa2fabf3deda76f1
fjohnson36 <fjohnson36@gatech.edu>
2015-11-10 16:10:50 -0500

    Update mongodb.server.controller.js
    h

--- app/controllers/mongodb.server.controller.js
+++ app/controllers/mongodb.server.controller.js
@@ -2,6 +2,39 @@ var config = require('../../config/config');
 var mongodb = require('mongodb');
 var fhir = require('fhir-node');
 
+exports.updateOnWatch = function(req, res) {
+
+	var Patient = require('mongoose').model('Patient');
+
+	var id = '{"id" : "' + req.patientId + '"}';
+	var query = JSON.parse(id);
+
+	var u = '{"onWatch" : ""}';
+	var updateData =  JSON.parse(u);
+	
+	if (req.onWatch == 'ifChecked') {
+		//u = u.replace('[checked]','checked');
+		//u = '{"onWatch : {"' + req.user.username + '" : "checked"}"}';
+		//u['onWatch'][req.user.username] = 'checked';
+		updateData.onWatch = 'checked'; //JSON.parse('[{' + req.user.username + ' : checked}]');
+	} else {
+		//u = u.replace('[checked]','');
+		//u = '{"onWatch : {"' + req.user.username + '" : ""}"}';
+		updateData.onWatch = ''; //JSON.parse('[{' + req.user.username + ': ""}]');
+	}
+	
+	//updateData.username = req.user.username;
+	
+	console.log(updateData);
+		
+	Patient.update(query, updateData, { multi: true }, function (err, raw) {
+	  if (err) return handleError(err);
+	  console.log('The raw response from Mongo was ', raw);
+	});
+	
+	res.send('ok');
+};
+
 exports.render = function(req, res) {
 	
 	var patientData = fhir();
@@ -12,21 +45,61 @@ exports.render = function(req, res) {
 		mongodb.MongoClient.connect(config.db, function(err, db) {
   
 			if(err) throw err;
-						
-		  	var patients = db.collection('patients');
+
+			console.log("Connected to Database");
+			
+			
+			var patientsdump = db.collection('patientsdump');
 
 			// remove the existing data
-			patients.drop(function (err) {
-				if(err) throw err;
+			patientsdump.drop(function (err) {
+				if(err) console.error(err);
 			});
 
-			
+
 			// bulk insert the new data
-		  	patients.insert(seedData, function(err, result) {
+			patientsdump.insert(seedData, function(err, result) {
+				if(err) console.error(err);
+				
+				var patients = db.collection('patients');
 
-				if(err) throw err;
+				// update patient data
+				
+				patientsdump.find().forEach(function(doc) {
 
-		  	});
+					console.log("patient insert start");
+	
+					var id = '{"id" : "' + doc.resource.id + '"}';
+					var query = JSON.parse(id);
+					var o = '{"upsert" : true}';
+					var option = JSON.parse(o);
+
+					var updateData =  JSON.parse(id);
+					updateData.name = doc.resource.name;
+					updateData.firstName = doc.resource.name[0].given[0];
+					updateData.lastName = doc.resource.name[0].family[0];
+					updateData.middleName = doc.resource.name[0].given[1];
+					updateData.gender = doc.resource.gender;
+					updateData.birthDate = doc.resource.birthDate;
+					updateData.address = doc.resource.address[0].line[0] + ', ' + 
+						doc.resource.address[0].city + ', ' + 
+						doc.resource.address[0].state;
+					updateData.active = doc.resource.active;
+
+					console.log(query);
+
+					patients.update(query, updateData, option, function (err, result) {
+						if(err) {
+							//throw err;
+							console.error(err);
+						}
+					});
+
+
+				});
+			});
+
+			console.log("patientsdump insertion done");
 			
 		});
 
