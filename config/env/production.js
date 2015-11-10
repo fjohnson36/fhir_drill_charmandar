157f169cc16dd0b06555a2c1adaf283c156fca27
fjohnson36 <fjohnson36@gatech.edu>
2015-11-10 16:26:01 -0500

    Update production.js

--- config/env/production.js
+++ config/env/production.js
@@ -3,4 +3,5 @@ module.exports = {
 	db: 'mongodb://charmander:fhir@ds045604.mongolab.com:45604/charmander',
 	fhir: 'http://polaris.i3l.gatech.edu:8080/gt-fhir-webapp-ro/base/',
 	sessionSecret: 'productionSessionSecret'
+	sessionTimeOutDuration: 300000
 }
