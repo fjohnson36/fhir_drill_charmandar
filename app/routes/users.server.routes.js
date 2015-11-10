a916a0f20d3875d890b384d60755e4b9de7e72be
fjohnson36 <fjohnson36@gatech.edu>
2015-11-10 16:17:56 -0500

    h

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,18 +1,32 @@
-var users = require('../../app/controllers/users.server.controller'),
+var users = require('../controllers/users.server.controller'),
 	passport = require('passport');
 
 module.exports = function(app) {
-	app.route('/signup')
+	app.route('/user/signup')
 		.get(users.renderSignup)
 		.post(users.signup);
 	
-	app.route('/signin')
+	app.route('/user/signin')
 		.get(users.renderSignin)
 		.post(passport.authenticate('local', {
 			successReturnToOrRedirect: '/',
-			failureRedirect: '/signin',
+			failureRedirect: '/user/signin',
 			failureFlash: true
-	}));
+		}),	function(req, res, next) {
+			// issue a remember me cookie if the option was checked
+			if (!req.body.remember_me) { return next(); }
+
+			var day = 24 * 60 * 60 * 1000;
+			res.cookie('charmander-userid', req.body.username, { maxAge: day });
+		
+		
+			issueToken(req.user, function(err, token) {
+      			if (err) { return next(err); }
+      			res.cookie('remember_me', token, { path: '/', httpOnly: true, maxAge: 604800000 });
+      			return next();
+    		});
+		
+		});
 	
 	app.get('/signout', users.signout);
 	
